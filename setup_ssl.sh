8a3430307132fbb592902afef93dc7272603ca0c
Sepanta7 <163876726+Sepanta7@users.noreply.github.com>
2024-09-20 20:37:02 +0330

    Update setup_ssl.sh

--- setup_ssl.sh
+++ setup_ssl.sh
@@ -5,6 +5,9 @@ mkdir -p /var/www/html/Get-ssl
 curl -o /var/www/html/Get-ssl/get_domain.php https://raw.githubusercontent.com/sepanta7/Get-ssl/main/get_domain.php
 curl -o /var/www/html/Get-ssl/issue_ssl.php https://raw.githubusercontent.com/sepanta7/Get-ssl/main/issue_ssl.php
 
+echo "Enter your domain or subdomain:"
+read domain
+
 echo "www-data ALL=(ALL) NOPASSWD: /usr/bin/certbot" | sudo tee -a /etc/sudoers
 
 echo "SSL setup script completed. You can now access the form via http://your-server-ip/Get-ssl/get_domain.php"
