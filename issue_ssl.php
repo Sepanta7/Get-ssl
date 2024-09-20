ed174311754e2cef92450c1637dc2adab6ce1b3c
Sepanta7 <163876726+Sepanta7@users.noreply.github.com>
2024-09-20 20:51:22 +0330

    Update issue_ssl.php
    fix bug

--- issue_ssl.php
+++ issue_ssl.php
@@ -1,26 +1,24 @@
 <?php
 if (isset($_GET['domain'])) {
     $domain = filter_var($_GET['domain'], FILTER_SANITIZE_STRING);
-    if (filter_var(gethostbyname($domain), FILTER_VALIDATE_IP)) {
-        $command = "sudo certbot --nginx -d " . escapeshellarg($domain) . " --non-interactive --agree-tos --email your-email@example.com";
+
+    if (!empty($domain)) {
+        $command = "sudo certbot certonly --webroot -w /var/www/html/ -d $domain --non-interactive --agree-tos -m your-email@example.com";
+        
+        // اجرای دستور Certbot برای دریافت SSL
         $output = shell_exec($command);
         
-        if ($output) {
-            echo "SSL certificate successfully issued for " . htmlspecialchars($domain) . "<br>";
-            echo "telegram:@sepanta_n<br>";
-            echo "GitHub:https://sepanta7";
+        if (strpos($output, 'Congratulations!') !== false) {
+            echo "SSL certificate successfully obtained for $domain.";
         } else {
-            echo "There was an error issuing SSL certificate for " . htmlspecialchars($domain);
-            echo "<br>telegram:@sepanta_n<br>";
-            echo "GitHub:https://sepanta7";
+            echo "Failed to obtain SSL certificate for $domain. Please check the logs for details.";
         }
+        
+        echo "<br>Telegram: @sepanta_n";
+        echo "<br>GitHub: https://sepanta7";
     } else {
-        echo "Invalid domain name.";
-        echo "<br>telegram:@sepanta_n<br>";
-        echo "GitHub:https://sepanta7";
+        echo "Invalid domain. Please go back and try again.";
     }
 } else {
-    echo "Domain is not set.";
-    echo "<br>telegram:@sepanta_n<br>";
-    echo "GitHub:https://sepanta7";
+    echo "No domain provided.";
 }
